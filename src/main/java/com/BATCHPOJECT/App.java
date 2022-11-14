ac232fa6588f6afc6d73736fd067e9132dcc6a8d
nelloruseshu <poojya2014@gmail.com>
2022-11-14 16:28:04 +0530

    initial commit by seshu

---
+++ src/main/java/com/BATCHPOJECT/App.java
@@ -0,0 +1,13 @@
+package com.BATCHPOJECT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
